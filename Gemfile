4862d0f2be85bf325e53526f4b48e583b09b257b
Vu Pham <vupham0326@gmail.com>
2015-06-30 01:39:24 -0700

    add figaro gem

--- Gemfile
+++ Gemfile
@@ -24,6 +24,12 @@ gem 'bootstrap-sass', '~> 3.3.5'
 
 gem "paperclip", "~> 4.2"
 
+gem "paperclip-dropbox", ">= 1.1.7"
+
+gem "figaro"
+
+
+
 group :production do
 	gem 'pg'
 	gem 'rails_12factor'
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ActiveRecord::Base
-	 has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/images/:style/default.jpg"
-	 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	
+
+	 has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
+						 	:storage => :dropbox,
+					    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+
+	  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
